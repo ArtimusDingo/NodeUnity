fbe84065f61669754531672fe10fd7fe7ed53757
MiniChickieRobots <60833142+ArtimusDingo@users.noreply.github.com>
2021-08-27 14:21:41 -0400

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microcontroller
+{
+    public static class NodeMCU
+    {
+        public const sbyte D0 = 16;
+        public const sbyte D1 = 5;
+        public const sbyte D2 = 4;
+        public const sbyte D3 = 0;
+        public const sbyte D4 = 2;
+        public const sbyte D5 = 14;
+        public const sbyte D6 = 12;
+        public const sbyte D7 = 13;
+        public const sbyte D8 = 15;
+        public const sbyte RX = 3;
+        public const sbyte TX = 1;
+        public const sbyte A0 = 17;
+    }
+}
---
+++ PacketHandler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace PacketHandling
+{
+    public struct Packet
+    {
+        public object value;
+        byte[] bytes;
+        
+
+        public Packet(int _type, byte[] _bytes)
+        {
+            value = Convert.ChangeType(PacketHandler.Dictionary[_type].DynamicInvoke(_bytes), PacketHandler.TypeDictionary[_type]);
+            bytes = _bytes;
+        }
+    }
+
+    public class PacketHandler
+    {
+        public static Dictionary<int, Delegate> Dictionary = new Dictionary<int, Delegate>();
+        public static Dictionary<int, Type> TypeDictionary = new Dictionary<int, Type>();
+     
+        public PacketHandler()
+        {
+            Dictionary[0] = new Func<byte[], int>(GetInt);
+            Dictionary[1] = new Func<byte[], float>(GetFloat);
+            Dictionary[2] = new Func<byte[], string>(GetString);
+            Dictionary[3] = new Func<byte[], string>(GetASCIIString);
+            Dictionary[4] = new Func<byte[], sbyte>(GetSByte);
+            TypeDictionary[0] = typeof(int);
+            TypeDictionary[1] = typeof(float);
+            TypeDictionary[2] = typeof(string);
+            TypeDictionary[3] = typeof(string);
+            TypeDictionary[4] = typeof(sbyte);
+        }
+
+        #region Private
+
+        private int GetInt(byte[] _bytes)
+        {
+            return BitConverter.ToInt32(_bytes, 0);
+        }
+
+        private float GetFloat(byte[] _bytes)
+        {
+            return BitConverter.ToSingle(_bytes, 0);
+        }
+
+        private string GetString(byte[] _bytes)
+        {
+            return BitConverter.ToString(_bytes);
+        }
+
+        private string GetASCIIString(byte[] _bytes)
+        {
+            return ASCIIEncoding.ASCII.GetString(_bytes);
+        }
+
+        private sbyte GetSByte(byte[] _bytes)
+        {
+         
+            return unchecked((sbyte)_bytes[0]);
+        }
+
+        #endregion
+
+        public byte[] GetByteSection(byte[] _allbytes, int _readsize, int Offset = 0)
+        {
+            byte[] message = new byte[_readsize];
+            Buffer.BlockCopy(_allbytes, Offset, message, 0, _readsize);
+            return message;
+        }
+
+        public byte[] AddBytes(byte[] _original, byte[] _addition)
+        {
+            int size = _original.Length + _addition.Length;
+            byte[] newbytes = new byte[size];
+            Buffer.BlockCopy(_original, 0, newbytes, 0, _original.Length);
+            Buffer.BlockCopy(_addition, 0, newbytes, _original.Length, _addition.Length);
+            return newbytes;
+        }
+
+
+
+    }
+}
